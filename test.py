086a7d7e2875bd5da9ef5ea08394d16e58f86dc6
SaranyaSittrarasu <saranya.karthik@outlook.com>
2025-10-15 12:47:27 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+age = 22
+if age >= 18 and age < 21:
+    print("Adult but not fully")
+elif age >= 21:
+    print("Fully adult")
+
+x = 10
+y = 3
+print("x + y =", x + y)
+print("x - y =", x - y)
+print("x * y =", x * y)
+print("x / y =", x / y)
+print("x // y =", x // y)  
+print("x % y =", x % y)    
+print("x ** y =", x ** y)  
